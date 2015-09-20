045c943f25939162766c76048c1cbbaf2530b6bd
Adrian CITU <citu_adrian@yahoo.com>
2015-09-20 17:58:45 +0200

    fixed comments

--- slae4/random-insertion-decoder2.asm
+++ slae4/random-insertion-decoder2.asm
@@ -10,66 +10,66 @@ _start:
 	jmp short call_shellcode
 
 decoder:
-       #get the adress of the shellcode
+       ;get the adress of the shellcode
        pop esi
        
-       #allign edi and esi
+       ;allign edi and esi
        lea edi, [esi]
 
   handle_next_block:
-       #check that the esi do not point
-       #to the terminator byte
+       ;check that the esi do not point
+       ;to the terminator byte
        xor ecx,ecx
        mov cl, byte[esi]
        mov bl , cl
        xor bl, 0xff
        
-       #if esi points to terminator byte
-       #then execute the shellcode
+       ;if esi points to terminator byte
+       ;then execute the shellcode
        jz short EncodedShellcode
 
-       #otherwise then ship next byte
-       #because it's the first byte
-       #of the block and it contains
-       #the number of bytes that
-       #the block contains.
+       ;otherwise then ship next byte
+       ;because it's the first byte
+       ;of the block and it contains
+       ;the number of bytes that
+       ;the block contains.
        inc esi
        
-       #dl it is used to count the
-       #number of bytes from a block
-       #already copied
+       ;dl it is used to count the
+       ;number of bytes from a block
+       ;already copied
        xor edx, edx
   handle_next_byte:
-       #check that the esi do not point
-       #to the terminator byte
+       ;check that the esi do not point
+       ;to the terminator byte
        mov bl, [esi]
        xor bl, 0xff
        
-       #if esi points toterminator byte
-       #then execute the shellcode
+       ;if esi points toterminator byte
+       ;then execute the shellcode
        jz short EncodedShellcode
        
-       #otherwise copy the byte pointed by
-       #esi to the location pointed by edi;
-       #esi is automatically incremented by
-       #the lodsb and edi by stosb
+       ;otherwise copy the byte pointed by
+       ;esi to the location pointed by edi;
+       ;esi is automatically incremented by
+       ;the lodsb and edi by stosb
        lodsb
        stosb
        
        inc dl
-       #check that all the bytes of the block
-       #have been copied;
-       #cl contains the first byte of the block
-       #representing the number of bytes of the
-       #block and dl contains the number of
-       #block bytes already copied
+       ;check that all the bytes of the block
+       ;have been copied;
+       ;cl contains the first byte of the block
+       ;representing the number of bytes of the
+       ;block and dl contains the number of
+       ;block bytes already copied
        cmp cl, dl
        
-       #if not zero then not all the block bytes
-       #have been copied
+       ;if not zero then not all the block bytes
+       ;have been copied
        jnz handle_next_byte
        
-       #otherwise go to the next block
+       ;otherwise go to the next block
        jmp handle_next_block
 call_shellcode:
 
